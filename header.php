b271588b5174ab02eca681b942c47cf3ea93dd5d
shubham.kanaujiya <shubham.kanaujiya@gingerwebs.co.in>
2023-09-16 14:26:12 +0530

    add header and footer

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1 id="main_title">Crud Application</h1>
+
+    <div class="container">
--- index.php
+++ index.php
@@ -1,49 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include 'header.php' ?>
 
-<head>
-    <title>Home Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-</head>
+<h2> All Students</h2>
+<table class="table table-hover table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Lat Name</th>
+            <th>Age</th>
+        </tr>
+        <tr>
+            <th>4</th>
+            <th>shubham</th>
+            <th>kanaujiya</th>
+            <th>25</th>
+        </tr>
+        <tr>
+            <th>5</th>
+            <th>anuj</th>
+            <th>kanaujiya</th>
+            <th>29</th>
+        </tr>
 
-<body>
-    <h1 id="main_title">Crud Application</h1>
+    </thead>
+    <tbody>
 
-    <div class="container">
-        <h2> All Students</h2>
-        <table class="table table-hover table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>First Name</th>
-                    <th>Lat Name</th>
-                    <th>Age</th>
-                </tr>
-                <tr>
-                    <th>4</th>
-                    <th>shubham</th>
-                    <th>kanaujiya</th>
-                    <th>25</th>
-                </tr>
-                <tr>
-                    <th>5</th>
-                    <th>anuj</th>
-                    <th>kanaujiya</th>
-                    <th>29</th>
-                </tr>
+    </tbody>
+</table>
 
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
-    </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+<?php include 'footer.php' ?>
