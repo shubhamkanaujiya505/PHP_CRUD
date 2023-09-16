0c54b338f3e53dc80667ddf464d8666eaa34fd56
shubham.kanaujiya <shubham.kanaujiya@gingerwebs.co.in>
2023-09-16 12:36:31 +0530

    connect database

---
+++ Database.php
@@ -0,0 +1,22 @@
+<?php 
+
+define("HOSTNAME", "localhost" );
+define("USERNAME", "root" );
+define("PASSWORD", "123456" );
+define("DATABASE", "crud_operation" );
+
+$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+
+if(!$connection){
+    die("Connection failed");
+}else{
+    echo "you are connected to database";
+}
+
+
+
+
+
+
+
+?>
